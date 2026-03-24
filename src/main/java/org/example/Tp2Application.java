a6a229eb9f563aae9f2053fc6f61157476d8ba71
gustalgebaile <gualgebaile@gmail.com>
2026-03-24 15:40:01 -0300

    Commit Start TP2

--- src/main/java/org/example/Tp2Application.java
+++ src/main/java/org/example/Tp2Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Tp1Application {
+public class Tp2Application {
     public static void main(String[] args) {
-        SpringApplication.run(Tp1Application.class, args);
+        SpringApplication.run(Tp2Application.class, args);
     }
 }
